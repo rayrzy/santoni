71f0137bc7d8a4bd91ce699ca3c6242db910c612
Hriday Sharma <hridaysharma42@gmail.com>
2017-09-02 01:28:04 +0000

    santoni: Build libmm-qcamera
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- device.mk
+++ device.mk
@@ -135,6 +135,7 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     camera.msm8937 \
+    libmm-qcamera \
     Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
