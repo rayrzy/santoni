5c73c98617756148ad7884d591f3d6eca4cafa73
Murat Kozan <muratkozan350@gmail.com>
2019-08-10 17:58:37 +0300

    santoni: rootdir: Update init.qcom.sh script
    * From xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.11.0.PDHMIXM:user/release-keys
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -84,7 +84,7 @@ start_msm_irqbalance_8939()
 {
 	if [ -f /vendor/bin/msm_irqbalance ]; then
 		case "$platformid" in
-		    "239" | "293" | "294" | "295" | "304" | "338" | "313" | "353" | "354")
+		    "239" | "293" | "294" | "295" | "304" | "313" | "353" | "354")
 			start vendor.msm_irqbalance;;
 		    "349" | "350" )
 			start vendor.msm_irqbal_lb;;
@@ -120,7 +120,7 @@ start_msm_irqbalance_atoll()
          fi
 }
 
-start_msm_irqbalance660()
+start_msm_irqbalance()
 {
 	if [ -f /vendor/bin/msm_irqbalance ]; then
 		case "$platformid" in
@@ -129,13 +129,6 @@ start_msm_irqbalance660()
 		    "318" | "327" | "385")
 			start vendor.msm_irqbl_sdm630;;
 		esac
-	fi
-}
-
-start_msm_irqbalance()
-{
-	if [ -f /vendor/bin/msm_irqbalance ]; then
-			start vendor.msm_irqbalance
 	fi
 }
 
@@ -235,7 +228,7 @@ case "$target" in
                   esac
                   ;;
        esac
-        start_msm_irqbalance660
+        start_msm_irqbalance
         ;;
     "apq8084")
         platformvalue=`cat /sys/devices/soc0/hw_platform`
@@ -406,7 +399,7 @@ case "$target" in
              hw_platform=`cat /sys/devices/system/soc/soc0/hw_platform`
         fi
         case "$soc_id" in
-             "336" | "337" | "347" | "360" | "393" )
+             "336" | "337" | "347" | "360" )
                   case "$hw_platform" in
                        "Surf")
                                     setprop qemu.hw.mainkeys 0
