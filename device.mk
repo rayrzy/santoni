ec0880e4ce6c97caf954d88021b880f532e75924
ZeeLog <Danjel.Zammit@gmail.com>
2020-04-01 13:36:36 +0200

    santoni: update GPS configs from LA.UM.8.6.r1
    - Set GPS parameters using gps_debug.conf
    - lower debug levels
    - build additional product packages
    - remove duplicate GPS flags from BC
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- device.mk
+++ device.mk
@@ -207,10 +207,12 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service
 
 # GPS
+PRODUCT_PACKAGES += \
+    libcurl
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps/gps_debug.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps_debug.conf \
     $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
     $(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
