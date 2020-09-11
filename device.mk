4b1961ed79134ad794477922ad40ea20a3de4fad
Jacob Deepu <d2jdeepu@gmail.com>
2020-09-11 16:45:43 +0530

    santoni: Switch back to source built biometrics HAL
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- device.mk
+++ device.mk
@@ -184,7 +184,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1 \
+    android.hardware.biometrics.fingerprint@2.1-service.santoni \
     libunwind
 
 # FM
